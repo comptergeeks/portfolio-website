8aa35bf065cce150593c6a85f7b48ee307cc2f11
comptergeeks <F@rhan0108>
2023-11-14 19:23:56 -0800

    final before uploading to github prior to development

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import itachi from "public/itachi.gif";
 
 export default function Home() {
   return (
     <div className={styles.farhanKhanParent}>
       <div className={styles.farhanKhan}>Farhan Khan</div>
-      <img className={styles.image1Icon} alt="" src="/itachi.gif" />
-      <div className={styles.comingSoon}>Coming Soon</div>
+        <Image className={styles.image1Icon} alt="" src={itachi}/>
+      <div className={styles.comingSoon}>Porfolio Coming Soon</div>
     </div>
   );
 }
